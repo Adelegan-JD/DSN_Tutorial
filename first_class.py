ea6989d5fa630a43870293acfe41ef8cfffe5627
Adelegan Deborah <deboahjesudemilade@gmail.com>
2026-02-22 18:00:06 +0100

    Added more content to my file

--- first_class.py
+++ first_class.py
@@ -1,2 +1,28 @@
-my_name = 'Deborah'
-print(my_name)
+# This is my first python file
+
+name1 = 'Deborah'  # This is a string
+my_name2 = "ADELEGAN" # This is a variable
+
+
+number1 = 3
+number2 = 4.6
+
+# Addition
+number = number1 + number2
+# print(number)
+# print(number)
+
+# Normal Division
+# print(6/3)
+
+# Floor Division
+# print(100//30)
+
+# Modular division
+# print(100 % 30)
+
+# Creating a string
+word_1 = 'This is a string'
+word_2 = 'This is Python string'
+
+print(word_2)
